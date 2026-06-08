f2c2a74a501fecaf239771c2295320d73de35284
Prafullsahu369 <155sahupraful@gmail.com>
2026-06-08 11:41:26 +0530

    Some chnages

--- Backend/routes/Subscriber.js
+++ Backend/routes/Subscriber.js
@@ -14,7 +14,7 @@ router.post('/', createSubscriber);
 // GET    /api/subscribe        → get all subscribers (admin panel)
 router.get('/',adminAuth, getAllSubscribers);
 
-// DELETE /api/subscribe/:id    → remove a subscriber (admin panel)
+// DELETE /api/subscribe/:id    
 router.delete('/:id',adminAuth, deleteSubscriber);
 
 module.exports = router;
